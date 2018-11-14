edb561918df0a77c252c868d65bf96e017f59ae1
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2018-11-14 09:56:51 -0500

    .

--- RakeTests/FunctionTest.cs
+++ RakeTests/FunctionTest.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Reflection;
 
 namespace RakeTests
 {
@@ -8,6 +10,23 @@ namespace RakeTests
         [TestMethod]
         public void SimpleXPath()
         {
+            var w = GetResource("simple-web.html");
+            var f = GetResource("simple-xpath.yaml");
+        }
+
+        private string GetResource(string resourceName)
+        {
+            var type = this.GetType();
+            var assembly = type.GetTypeInfo().Assembly;
+            var fullResourceName = $"{type.Namespace}.Functions.{resourceName}";
+
+            using (var stream = assembly.GetManifestResourceStream(fullResourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                var text = reader.ReadToEnd();
+
+                return text;
+            }
         }
     }
 }
