a280fad3ba9d954d3cc046e13c1d2629847c510f
vplauzon <vplauzon@vplauzon.com>
2018-11-19 18:35:46 -0500

    .

---
+++ RakeLib/CompiledFunction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RakeLib
+{
+    public class CompiledFunction
+    {
+        public string[] Inputs { get; set; }
+
+        public NamedCompiledCompute[] Computes { get; set; }
+    }
+}
