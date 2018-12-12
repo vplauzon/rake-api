edcbe6566ff47f0204024f30500968f94a67d3c8
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2018-12-11 16:19:51 -0800

    Failed test because of no equals implemented

--- RakeTests/CompilerTest.cs
+++ RakeTests/CompilerTest.cs
@@ -78,5 +78,29 @@ namespace RakeTests
             Assert.AreEqual(description.Inputs.Length, compiled.InputNames.Length, "Inputs");
             Assert.AreEqual(2, compiled.Computes.Length, "Computes");
         }
+
+        [TestMethod]
+        public async Task InputToVariableTo2Outputs()
+        {
+            var description = new FunctionDescription
+            {
+                Inputs = new[] { "url" },
+                Variables = new Dictionary<string, string>()
+                {
+                    {"copyUrl", "url" }
+                },
+                Outputs = new Dictionary<string, string>()
+                {
+                    {"outThere", "url" },
+                    {"outThere2", "copyUrl" }
+                }
+            };
+            var compiler = new Compiler();
+            var compiled = await compiler.CompileAsync(description);
+
+            Assert.IsNotNull(compiled);
+            Assert.AreEqual(description.Inputs.Length, compiled.InputNames.Length, "Inputs");
+            Assert.AreEqual(2, compiled.Computes.Length, "Computes");
+        }
     }
 }
